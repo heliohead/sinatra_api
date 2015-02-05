a30c7e9e800c1d80262dddb42d462f98e371f812
Helio Oliveira <hheelliioo@live.com>
2015-02-05 15:50:38 -0200

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-
+Cab.create!(name: 'Antony', email: 'antony@beatles.com', password: '1234', address: 'Av Luis Faccini')
+Cab.create!(name: 'Flea', email: 'flea@beatles.com', password: '1234', address: 'Av Francisco Matarazo')
+Cab.create!(name: 'Chad', email: 'chad@beatles.com', password: '1234', address: 'Rua Tuiuti')
+Cab.create!(name: 'John', email: 'john@beatles.com', password: '1234', address: 'Av Moema')
